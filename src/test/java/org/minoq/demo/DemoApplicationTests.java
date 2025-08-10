274bc33ecb1534112d61e8e2dec0c8a3e28b9293
Michael Quarcoo <michaelquarcoo04@gmail.com>
2025-08-10 07:48:12 +0000

    Project Init

---
+++ src/test/java/org/minoq/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.minoq.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
