8d869e93d1a0cc47543953a8c3696aa4b69857be
Michael Quarcoo <michaelquarcoo04@gmail.com>
2025-08-10 09:04:44 +0000

    feat: add rest controller with site-health endpoint

---
+++ src/main/java/org/minoq/demo/rest/DemoRestController.java
@@ -0,0 +1,15 @@
+package org.minoq.demo.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class DemoRestController {
+
+    @GetMapping
+    public String showSiteHealth () {
+        return "App is Live!!";
+    }
+}
